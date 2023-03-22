86fb2a40959c43a30c47d9768f72e2b8344d44bc
barathrajd <barathdeva757@gmail.com>
2023-03-22 09:26:16 +0530

    number formatter

--- main.js
+++ main.js
@@ -3,11 +3,18 @@
 const dateFormatter = new Intl.DateTimeFormat('en-in', {
   dateStyle: 'short',
 });
-console.log(dateFormatter.format(new Date()));
+// console.log(dateFormatter.format(new Date()));
 
 const relativeTimeFormatter = new Intl.RelativeTimeFormat(
   undefined,
   { numeric: 'auto' }
 );
 
-console.log(relativeTimeFormatter.format(10, 'day'));
+// console.log(relativeTimeFormatter.format(10, 'day'));
+
+const numberFormatter = new Intl.NumberFormat(undefined, {
+  style: 'currency',
+  currency: 'INR',
+});
+
+console.log(numberFormatter.format(10000));
